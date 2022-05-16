ab28182d7e25240c3473da48603d6117ac464601
Medine Kaya <99620226+karojjj@users.noreply.github.com>
2022-05-16 12:02:54 -0400

    added some codes

--- src/com/syntax/Batch12.java
+++ src/com/syntax/Batch12.java
@@ -4,5 +4,7 @@ public class Batch12 {
 
     public static void main(String[] args) {
         System.out.println("ajdjaskdlaskd");
+        System.out.println("jksahdjka");
+        System.out.println("kjahkjhsdiuaHUDH");
     }
 }
